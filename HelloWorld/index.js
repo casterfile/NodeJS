2bae3a33f2738424608020a6777e973f4b97e066
Anthony Castor <Anthony Castor>
2017-04-29 23:05:10 +0800

    Tony Project 1

--- HelloWorld/index.js
+++ HelloWorld/index.js
@@ -1,6 +1,10 @@
 var http = require('http'); //add the http module
 var myServer = http.createServer(function(request, response){
-	response.writeHead(200, {"Content-Type" : "text/plain"});
-	response.write("Hello");
+	//response.writeHead(200, {"Content-Type" : "text/plain"});
+	//response.write("Hello World");
+	response.writeHead(200, {"Content-Type" : "text/html"});
+	response.write("<b>Hello</b> World");
 	response.end();
 });// create server
+
+myServer.listen(3000);
