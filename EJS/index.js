aa00c190bd4ab89e575a61da50a3d43c04810fc8
Anthony Castor <Anthony Castor>
2017-04-30 00:39:06 +0800

    Tony Project 4

--- EJS/index.js
+++ EJS/index.js
@@ -1,31 +1,35 @@
 var express = require('express');
 var app = express();
 
-app.set('view engine', 'ejs');
+app.set('view engine', 'ejs');//this will access the view folder
+// app.set('views', __dirname+'/newviews');//how to change the views
 
 app.get('/', function(req, res){
-	res.send('<h1>Hello</h1> express');
+	res.render('default', {
+	title: 'Home',
+	users: ['Ray', 'Kevin','Tony']
+	});//getting the view default.ejs
 });
 
-app.get('/me', function(req, res){
-	res.send('facebook/AnthonyACastor');
-});
+// app.get('/me', function(req, res){
+// 	res.send('facebook/AnthonyACastor');
+// });
 
-app.get('/who/:name?', function(req, res){
-	var name = req.params.name; //http://localhost:3000/who/tony
-	res.send(name + ' was here!');
-});
+// app.get('/who/:name?', function(req, res){
+// 	var name = req.params.name; //http://localhost:3000/who/tony
+// 	res.send(name + ' was here!');
+// });
 
-app.get('/who/:name?/:title?', function(req, res){
-	var name = req.params.name; //http://localhost:3000/who/tony
-	var title = req.params.title;// http://localhost:3000/who/tony/boss
-	res.send('<p>name: '+ name + '<br>title: '+ title + '</p>');
-});
+// app.get('/who/:name?/:title?', function(req, res){
+// 	var name = req.params.name; //http://localhost:3000/who/tony
+// 	var title = req.params.title;// http://local host:3000/who/tony/boss
+// 	res.send('<p>name: '+ name + '<br>title: '+ title + '</p>');
+// });
 
 
-app.get('*', function(req, res){
-	res.send('Bad Route');
-});
+// app.get('*', function(req, res){
+// 	res.send('Bad Route');
+// });
 
 
 var server = app.listen(3000, function(){
