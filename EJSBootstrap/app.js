690cd66c6566971d94e010730676884b0c51f872
Anthony Castor <Anthony Castor>
2017-04-30 12:09:20 +0800

    Tony Project 5

--- EJSBootstrap/app.js
+++ EJSBootstrap/app.js
@@ -1,25 +1,14 @@
 var express = require('express');
 var app = express();
+var routes = require('./routes');//Incude Routes Folder
 
 app.set('view engine', 'ejs');//this will access the view folder
 // app.set('views', __dirname+'/newviews');//how to change the views
 
-app.get('/', function(req, res){
-	res.render('default', {
-	title: 'Home',
-	classname: 'home',
-	users: ['Ray', 'Kevin','Tony']
-	});//getting the view default.ejs
-});
+app.locals.pagetitle = "Awesome Website ";
 
-app.get('/about', function(req, res){
-	res.render('default', {
-	title: 'About Me',
-	classname: 'about',
-	users: ['Ray', 'Kevin','Tony']
-	});//getting the view default.ejs
-});
-
+app.get('/', routes.index);
+app.get('/about', routes.about);
 
 app.get('*', function(req, res){
 	res.send('Bad Route');
